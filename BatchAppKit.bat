982434785d9910962bb1b6b9ac9de597f69ab1fd
SteveSfara <80094756+SteveSfara@users.noreply.github.com>
2021-03-05 10:33:29 +0200

    Add files via upload

---
+++ BatchAppKit.bat
@@ -0,0 +1,69 @@
+
+@echo off
+cls
+goto guide
+
+:guide
+echo This is the guide:
+echo There is a limit on the scripting.
+echo There will be a new limit every single update.
+echo The app has it's own coding.
+echo.
+echo.
+set /pause Press any key to continue.
+goto start
+:start
+cls
+echo Type the name of the program: (including ".bat")
+set /p projectname=
+cls
+goto code
+
+:code
+cls
+set /p c=
+set /p co=
+set /p cod=
+set /p code=
+set /p codes=
+set /p codesu=
+set /p codesus=
+set /p codesusi=
+set /p codesusia=
+set /p codesusian=
+set /p codesusiane=
+set /p codesusianeg=
+set /p codesusianegr=
+set /p codesusianegru=
+set /p codesusianegrui=
+set /p codesusianegruir=
+set /p codesusianegruirs=
+set /p codesusianegruirsa=
+set /p codesusianegruirsay=
+set /p codesusianegruirsayy=
+set /p codesusianegruirsayye=
+set /p codesusianegruirsayyey=
+cls
+echo Saving...
+echo %c% >>%projectname%
+echo %co% >>%projectname%
+echo %cod% >>%projectname%
+echo %code% >>%projectname%
+echo %codes% >>%projectname%
+echo %codesu% >>%projectname%
+echo %codesus% >>%projectname%
+echo %codesusi% >>%projectname%
+echo %codesusian% >>%projectname%
+echo %codesusiane% >>%projectname%
+echo %codesusianeg% >>%projectname%
+echo %codesusianegr% >>%projectname%
+echo %codesusianegru% >>%projectname%
+echo %codesusianegrui% >>%projectname%
+echo %codesusianegruir% >>%projectname%
+echo %codesusianegruirs% >>%projectname%
+echo %codesusianegruirsa% >>%projectname%
+echo %codesusianegruirsay% >>%projectname%
+echo %codesusianegruirsayy% >>%projectname%
+echo %codesusianegruirsayye% >>%projectname%
+echo %codesusianegruirsayyey% >>%projectname%
+exit
